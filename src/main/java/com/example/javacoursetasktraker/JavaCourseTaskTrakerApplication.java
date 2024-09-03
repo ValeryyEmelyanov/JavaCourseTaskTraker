d95d91352d20c960b6f55e0cdb2c683c7b61ef3c
ValeryyEmelyanov <130102631+ValeryyEmelyanov@users.noreply.github.com>
2024-09-04 01:00:53 +0300

    Create

---
+++ src/main/java/com/example/javacoursetasktraker/JavaCourseTaskTrakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacoursetasktraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourseTaskTrakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCourseTaskTrakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javacoursetasktraker/JavaCourseTaskTrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacoursetasktraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCourseTaskTrakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
